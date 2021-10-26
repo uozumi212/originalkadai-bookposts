ff0c4e130a6ffb4e0790d5ae484cb5164cbb267d
kazuma.uozumi <kazu3278mana@gmail.com>
2021-10-26 11:00:59 +0000

    bookposts

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get "login", to: "sessions#new"
     post "login", to: "sessions#create"
     delete "logout", to: "sessions#destroy"
-    get "bookposts/new", to: "bookposts#new"
+    
    
     get "signup", to: "users#new"
      
