c334cae693d32ed3afedd03554e3b365f997c48e
kazuma.uozumi <kazu3278mana@gmail.com>
2021-10-27 03:02:08 +0000

    bookposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 10 }
+    validates :name, presence: true, length: { maximum: 20 }
      validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
